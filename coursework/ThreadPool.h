dda3c8952eda5db83941669edbd2d786fa6b0dc8
Olga Zaykova <olya.sliva1@ya.ru>
2020-05-14 23:23:43 +0300

    broaken concurrency

---
+++ coursework/ThreadPool.h
@@ -0,0 +1,14 @@
+//
+// Created by Olga Zaikova on 2020-05-14.
+//
+
+#ifndef COURSEWORK_THREADPOOL_H
+#define COURSEWORK_THREADPOOL_H
+
+
+class ThreadPool {
+
+};
+
+
+#endif //COURSEWORK_THREADPOOL_H
